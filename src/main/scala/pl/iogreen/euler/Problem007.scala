e5683288c7852de9db045d75d0a646c8303d4029
Blazej Bucko <blazej.bucko@iogreen.pl>
2012-01-29 23:06:27 +0100

    Problem 007

---
+++ src/main/scala/pl/iogreen/euler/Problem007.scala
@@ -0,0 +1,16 @@
+package pl.iogreen.euler
+
+
+class Problem007 {
+
+  def atkinSieve(n: Int): List[Int] = {
+    var result: List[Int] = 5 :: 3 :: 2 :: Nil
+    var sieve: List[Boolean] = List.fill(n)(false)
+    result
+  }
+
+  def resolve(n: Int): Int = {
+    42
+  }
+
+}
---
+++ src/test/scala/pl/iogreen/euler/Problem007Test.scala
@@ -0,0 +1,15 @@
+package pl.iogreen.euler
+
+import org.scalatest.FunSuite
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class Problem007Test extends FunSuite {
+
+  val resolver = new Problem007()
+
+  test("generating primes") {
+
+  }
+}
