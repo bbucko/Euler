a533d66967176a7baaca8f0eaed159250dd13bcd
Blazej Bucko <blazej.bucko@iogreen.pl>
2012-01-29 18:13:24 +0100

    Implemented faster algorithm

--- src/main/scala/pl/iogreen/euler/Problem003.scala
+++ src/main/scala/pl/iogreen/euler/Problem003.scala
@@ -1,41 +1,63 @@
 package pl.iogreen.euler
 
 import collection.mutable.ListBuffer
+import collection.immutable.Range.Inclusive
+import collection.immutable.Range
 
 class Problem003 {
 
 
-  def primesSieve(n: Double): List[Int] = {
-    val sieve: ListBuffer[Boolean] = ListBuffer.fill(n.toInt + 1)(false)
-
-    for (i <- Iterator.range(2, scala.math.sqrt(n).toInt + 1)) {
-      if (!sieve(i)) {
-        for (j <- (2 * i).to(n.toInt, i)) {
-          sieve.update(j, true)
+  def primesSieve(n: Long): List[Int] = {
+    if (n == 1) return 1 :: Nil
+    println("Looking for primes less then: " + n)
+    val maxPrime: Int = n.toInt
+    val sieve: ListBuffer[Boolean] = ListBuffer.fill(maxPrime + 1)(true)
+    val iRange: Range = 2.until(maxPrime / 2)
+    for (i <- iRange) {
+      if (sieve(i)) {
+        val jRange: Inclusive = (2 * i).to(maxPrime, i)
+        for (j <- jRange) {
+          sieve.update(j, false)
         }
       }
     }
 
     var primes: List[Int] = Nil
     sieve.zipWithIndex foreach {
-      case (value, index) => if (!value && index > 1) primes = index :: primes
+      case (value, index) => if (value && index > 1) primes = index :: primes
     }
+
     primes.reverse
   }
 
+  def primeFactors(i: Long): List[Int] = {
+    val primes = primesSieve((scala.math.sqrt(i)).toInt)
 
-  def primeFactors(i: Int): List[Int] = {
-    if (i == 1) return i :: Nil
-
-    val primes: List[Int] = primesSieve(scala.math.sqrt(i) + 1)
     var foundPrimeFactors: List[Int] = Nil
+    for (n <- primes) {
+      if ((n * n) <= i) {
+        if (i % n == 0)
+          foundPrimeFactors = n :: foundPrimeFactors
+      }
+    }
 
-    foundPrimeFactors
+    foundPrimeFactors.reverse
   }
 
-
-  def resolve(n: BigInt): Long = {
-    29
+  def resolve(max: Long): Long = {
+    var factor = 2
+    var lastFactor = 1
+    var n = max
+    while (n > 1) {
+      if (n % factor == 0) {
+        lastFactor = factor
+        n /= factor
+        while (n % factor == 0) {
+          n /= factor
+        }
+      }
+      factor += 1
+    }
+    lastFactor
   }
-
 }
