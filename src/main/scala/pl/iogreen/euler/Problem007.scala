b76dceda7b2e86fda9c01d3997c0170b60531d42
Blazej Bucko <blazej.bucko@iogreen.pl>
2012-01-30 22:54:03 +0100

    Solved Problem 007

--- src/main/scala/pl/iogreen/euler/Problem007.scala
+++ src/main/scala/pl/iogreen/euler/Problem007.scala
@@ -1,16 +1,74 @@
 package pl.iogreen.euler
 
+import collection.mutable.ListBuffer
+
 
 class Problem007 {
 
-  def atkinSieve(n: Int): List[Int] = {
-    var result: List[Int] = 5 :: 3 :: 2 :: Nil
-    var sieve: List[Boolean] = List.fill(n)(false)
-    result
+  def atkinSieve(limit: Int): List[Int] = {
+    var result: List[Int] = Nil
+
+    val sqareRoot: Int = scala.math.sqrt(limit).toInt
+    val primes: ListBuffer[Boolean] = ListBuffer.fill(limit + 1)(false)
+
+    for (x <- 1 until sqareRoot; y <- 1 until sqareRoot) {
+      var n = 4 * x * x + y * y;
+      if (n <= limit && (n % 12 == 1 || n % 12 == 5)) {
+        primes(n) ^= true
+      }
+
+      n = 3 * x * x + y * y;
+      if (n <= limit && n % 12 == 7) {
+        primes(n) ^= true
+      }
+
+      n = 3 * x * x - y * y;
+      if (x > y && n <= limit && n % 12 == 11) {
+        primes(n) ^= true
+      }
+    }
+
+    for (n <- 5 until sqareRoot) {
+      if (primes(n)) {
+        val s = n * n;
+        for (k <- s to(limit, s)) {
+          primes(k) = false
+        }
+      }
+    }
+
+    for (n <- 5 to(limit, 2)) {
+      if (primes(n) && isPrime(n)) {
+        result = n :: result
+      }
+    }
+
+    2 :: 3 :: result.reverse
+  }
+
+  def isPrime(n: Long): Boolean = {
+    val ceil = math.sqrt(n).longValue()
+    var m = 2L
+    while (m <= ceil) {
+      if (n % m == 0) {
+        return false
+      }
+      m += 1
+    }
+    true
   }
 
   def resolve(n: Int): Int = {
-    42
+    var primeCount = 1
+    var lastNumber = 2
+
+    while (primeCount < n) {
+      lastNumber += 1
+      if (isPrime(lastNumber)) {
+        primeCount += 1
+      }
+    }
+    lastNumber
   }
 
 }
--- src/test/scala/pl/iogreen/euler/Problem007Test.scala
+++ src/test/scala/pl/iogreen/euler/Problem007Test.scala
@@ -10,6 +10,24 @@ class Problem007Test extends FunSuite {
   val resolver = new Problem007()
 
   test("generating primes") {
+    assert(resolver.atkinSieve(5) === 2 :: 3 :: 5 :: Nil)
+    assert(resolver.atkinSieve(5).size === 3)
 
+    assert(resolver.atkinSieve(10) === 2 :: 3 :: 5 :: 7 :: Nil)
+    assert(resolver.atkinSieve(10).size === 4)
+
+    assert(resolver.atkinSieve(110000)(10000) === 104743)
   }
+
+  test("isPrime") {
+    assert(resolver.isPrime(5) === true)
+    assert(resolver.isPrime(6) === false)
+    assert(resolver.isPrime(103843) === true)
+  }
+
+  test("resolve") {
+    assert(resolver.resolve(6) === 13)
+    assert(resolver.resolve(10001) === 104743)
+  }
+
 }
